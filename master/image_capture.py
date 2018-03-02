8bd25cfc2b30384ae336aae7bde41cbe1e6ecd2e
Pietro Rignanese <pietro_rignanese@hotmail.it>
2018-03-02 05:49:06 +0100

    Update image_capture.py

--- master/image_capture.py
+++ master/image_capture.py
@@ -6,12 +6,13 @@ from picamera import PiCamera
 import time
 import date_hour 
 
+#Take a picture
 def get_img():
-    print("Get photo...")
+    print(" -+-+-+- Get photo +-+-+-")
     camera = PiCamera()
     camera.start_preview()
     camera.annotate_text = date_hour.date_hour_now()
     time.sleep(5)
-    camera.capture('Trap/%s.jpg' % date_hour.date_hour_now())
+    camera.capture('Trap/%s.jpg' % date_hour.date_hour_now()) #the photo will be renamed based on the time
     camera.stop_preview()
-    print("Photo OK!")
+    print("--- Photo OK! ---")
